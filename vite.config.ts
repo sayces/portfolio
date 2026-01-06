262128db993abf94a00dcc448b31374908439793
sayces <sayces@mail.ru>
2026-01-06 22:59:32 +0700

    try 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()], 
-  base: '/portfolio/',
+  plugins: [react(), tailwindcss()],
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
 });
