a2c57687df9cbff70e90e79603b59d1345b1526f
sayces <sayces@mail.ru>
2026-01-22 19:14:06 +0300

    delete postcss config

--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    '@csstools/postcss-oklab-function': {
-      preserve: false, // Конвертирует oklch в rgb
-    },
-    autoprefixer: {},
-  },
-}
